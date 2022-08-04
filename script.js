25646ccaf6faccc0526a99fcd4e60dbb9cc048ee
Kyle Morgan <kfwmorgan@gmail.com>
2022-08-04 11:25:15 -0500

    created basic file structure boilerplate for html, added a background color for css, and laid out the algorithm for the JS file

---
+++ script.js
@@ -0,0 +1,10 @@
+//promt user with the premise of the game
+//set game counter to 0 
+//ask user to enter a selection 
+//validate users selection 
+//radom generate computers selection 
+//compare players variables looping through win conditions
+//incrament winners tally by 1 
+//check to see if either side has won 5 games
+//if yes end game and announce winner
+//if no loop back throught the game until victory condition has been met.
